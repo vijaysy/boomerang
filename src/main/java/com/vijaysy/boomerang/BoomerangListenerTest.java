b7f13a55511db008c787cfb52e60c86637af395e
vijaysy <vijayyalasangimath@gmail.com>
2016-04-02 23:03:56 +0530

    reff

--- src/main/java/com/vijaysy/boomerang/BoomerangListenerTest.java
+++ src/main/java/com/vijaysy/boomerang/BoomerangListenerTest.java
@@ -5,7 +5,7 @@ import com.vijaysy.boomerang.listeners.SubListenerThread;
 /**
  * Created by vijay.yala on 02/04/16.
  */
-public class BoomerangListener {
+public class BoomerangListenerTest {
     public static void main(String [] args){
         SubListenerThread subListenerThread = new SubListenerThread();
         Thread thread = new Thread(subListenerThread);

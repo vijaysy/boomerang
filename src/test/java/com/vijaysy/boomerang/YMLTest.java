4d9cdf360c975f6704bff0f3758ee8a2a41f8d2e
vijaysy <vijayyalasangimath@gmail.com>
2016-04-05 18:30:39 +0530

    basic log enabled

--- src/test/java/com/vijaysy/boomerang/YMLTest.java
+++ src/test/java/com/vijaysy/boomerang/YMLTest.java
@@ -3,6 +3,7 @@ package com.vijaysy.boomerang;
 import com.esotericsoftware.yamlbeans.YamlReader;
 import com.vijaysy.boomerang.models.Config.ListenerConfig;
 import com.vijaysy.boomerang.models.Config.ThreadConfig;
+import lombok.extern.slf4j.Slf4j;
 
 import java.io.FileReader;
 import java.util.HashMap;
@@ -12,19 +13,21 @@ import java.util.Map;
 /**
  * Created by vijaysy on 04/04/16.
  */
+@Slf4j
 public class YMLTest {
     public static void main(String [] args) throws Exception{
         YamlReader reader = new YamlReader(new FileReader("config/boomerangListener.yml"));
         Object object = reader.read();
         Map map = (Map)object;
-        System.out.printf(map.toString());
+        //System.out.printf(map.toString());
         ListenerConfig listenerConfig = new ListenerConfig();
 
         listenerConfig.setThreadConfigs((List<ThreadConfig>) map.get("threadConfigs"));
         listenerConfig.setGroupName((String)map.get("groupName"));
         listenerConfig.setRedisConfig((HashMap<String, String>) map.get("redisConfig"));
-
-        System.out.printf(listenerConfig.toString());
+        log.info("Testing log");
+        log.info(""+listenerConfig);
+        //System.out.printf(listenerConfig.toString());
 
 
     }

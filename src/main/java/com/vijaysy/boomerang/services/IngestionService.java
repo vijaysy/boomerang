e94b9ff658d7486a751a8aa0d002876ff198c341
vijaysy <vijayyalasangimath@gmail.com>
2016-04-11 13:30:59 +0530

    changes

--- src/main/java/com/vijaysy/boomerang/services/IngestionService.java
+++ src/main/java/com/vijaysy/boomerang/services/IngestionService.java
@@ -6,5 +6,5 @@ import com.vijaysy.boomerang.models.RetryItem;
  * Created by vijaysy on 08/04/16.
  */
 public interface IngestionService {
-    public void process(RetryItem retryItem) throws Exception;
+    void process(RetryItem retryItem) throws Exception;
 }

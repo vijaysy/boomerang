8d446b50f1ecc60900bcf8d74b63947e0f32ea89
vijaysy <vijayyalasangimath@gmail.com>
2016-04-27 16:19:47 +0530

    pac in test

--- src/test/java/com/vijaysy/boomerang/utils/MainTest.java
+++ src/test/java/com/vijaysy/boomerang/utils/MainTest.java
@@ -1,11 +1,62 @@
 package com.vijaysy.boomerang.utils;
 
+import java.util.concurrent.ArrayBlockingQueue;
+import java.util.concurrent.BlockingQueue;
+
 /**
  * Created by vijaysy on 26/04/16.
  */
 public class MainTest {
 
     public static void main(String[] args) {
-        new Thread(() -> {System.out.printf("Hi");}).start();
+        //new Thread(() -> {System.out.printf("Hi");}).start();
+        BlockingQueue<String> blockingQueue = new ArrayBlockingQueue<>(3);
+        new Thread(() -> {
+            for (int i = 0; i < 10; i++) {
+                try {
+                    Thread.sleep(i);
+                    blockingQueue.put(String.format("%s",i));
+                    System.out.println("Produced:" + i);
+
+                } catch (InterruptedException e) {
+                    e.printStackTrace();
+                }
+            }
+            try {
+                blockingQueue.put("exit");
+            } catch (InterruptedException e) {
+                e.printStackTrace();
+            }
+        }).start();
+
+
+        new Thread(() -> {
+            String string;
+            try {
+                while ((string = blockingQueue.take()) != "exit") {
+                    Thread.sleep(10);
+                    System.out.println("Consumed: " + string);
+                }
+
+            } catch (InterruptedException e) {
+                e.printStackTrace();
+            }
+
+
+        }).start();
+
     }
+
+    static class MyFlag{
+        boolean f;
+
+        public boolean isF() {
+            return f;
+        }
+
+        public void setF(boolean f) {
+            this.f = f;
+        }
+    }
+
 }
